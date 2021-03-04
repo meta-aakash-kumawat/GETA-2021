e5ea031bd22776b3e9a775fd213302e1f311bd0c
meta-aakash-kumawat <79217824+meta-aakash-kumawat@users.noreply.github.com>
2021-03-04 14:52:10 +0530

    Update Main.java

--- JobScheduler FCFS/Main.java
+++ JobScheduler FCFS/Main.java
@@ -7,19 +7,19 @@ public class Main
 		
 		int processes[][] = { {0,10}, {6,20}, {60,10}, {110,5} };
 		
-		int completionTime[]  = job.completionTime(processes);
-		int waitingTime[] = job.waitingTime(processes);
-		int turmAroundTime[] = job.turnAroundTime(processes);
+		int completionTime[]  = job.getCompletionTime(processes);
+		int waitingTime[] = job.getWaitingTime(processes);
+		int turmAroundTime[] = job.getTurnAroundTime(processes);
 		
 		for(int i = 0; i < processes.length; i++)
 		{
 			System.out.println("Process " +i);
-			System.out.println("Completion time " +completionTime[i]);
-			System.out.println("Waiting time " +waitingTime[i]);
-			System.out.println("Turn Around Time " +turmAroundTime[i]);
+			System.out.println("Completion time " +getCompletionTime[i]);
+			System.out.println("Waiting time " +getWaitingTime[i]);
+			System.out.println("Turn Around Time " +getTurmAroundTime[i]);
 			System.out.println();
 		}
-		System.out.println("Average waiting time " +job.avgWaitingTime(processes));
+		System.out.println("Average waiting time " +job.getAvgWaitingTime(processes));
 		System.out.println();
 		System.out.println("Maximum wating time " +job.maximumWitingTime(processes));
 	}
