95cc445ab72f8446d5abe5bb68e7023f751a239a
meta-aakash-kumawat <79217824+meta-aakash-kumawat@users.noreply.github.com>
2021-03-04 15:00:54 +0530

    Update LCMandHCFMain.java

--- LCMandHCF/LCMandHCFMain.java
+++ LCMandHCF/LCMandHCFMain.java
@@ -13,12 +13,25 @@ public class LCMandHCFMain
 		assertEquals(75,obj.lcm(25, 3));
 	}
 	
+	@Test(expected = AssertionError.class)
+	public void CheckForNegativeValues()
+	{
+		LCMandHCF obj = new LCMandHCF();
+		assertTure(obj.lcm(-7, 20));	
+	}
 	@Test
 	public void HCFShouldReturnValue()
 	{
 		LCMandHCF obj = new LCMandHCF();
 		assertEquals(5,obj.hcf(10, 15));
 		assertEquals(1,obj.hcf(31,2));
+	}
+	
+	@Test(expected = AssertionError.class)
+	public void CheckForNegativeValuesHCF()
+	{
+		LCMandHCF obj = new LCMandHCF();
+		assertTure(obj.hcf(-7, 0));	
 	}
 
 }
