d7af194819dccc04f6293a3402f5f739d6f0d1d9
meta-aakash-kumawat <79217824+meta-aakash-kumawat@users.noreply.github.com>
2021-03-14 21:54:33 +0530

    Inheritance-and-Composition

---
+++ Inheritance and Compostion/Animal.java
@@ -0,0 +1,11 @@
+public abstract class Animal 
+{
+	public String name;
+	public String animalType;
+	public String sound;
+	public int age;
+	public Double weight;
+	public String uniqueIdOfAnimal;
+		
+	public abstract String getAnimalSound();
+}
